490e2b08659a4d015ce7db45e4c0f7d0b23e1b70
oakes680 <chris.oakes@outlook.com>
2019-12-12 14:48:44 -0800

    chris-oakes

--- src/components/NasaCard/NasaCard.js
+++ src/components/NasaCard/NasaCard.js
@@ -1,14 +1,32 @@
 import React from "react";
-
+import { Card, CardBody, CardImg, CardTitle, CardSubtitle, CardText, Button } from "reactstrap";
 
 
 const NasaCard = (props) => {
   return (
-    <div className='earth-card'>
-     
-      <img src={props.eimg} alt='cool pic' /> 
+    <div >
+      <Card className="mt-4">
+        <CardImg
+          width="10%"
+          height= '100vh'
+          src={props.eimg}
+          alt="Card image cap"
+          style={{ maxWidth: "200px" }}
+        />
+        <CardBody>
+          <CardTitle>{props.eID}</CardTitle>
+          <CardSubtitle >Earth Date: {props.eDate}</CardSubtitle>
+          {/* <Button>Button</Button> */}
+        </CardBody>
+      </Card>
     </div>
   )
 };
 
 export default NasaCard
+
+
+
+
+// className = 'earth-card'
+  // < img src = { props.eimg } alt = 'cool pic' /> 
---
+++ src/components/StyledComponents/Styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 850px;
+  margin: 20px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`
+
+
+
+
+// .entry {
+//   display: flex;
+//   justify - content: space - between;
+//   flex - wrap: wrap;
+// }
